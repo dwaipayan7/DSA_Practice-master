e525809f3377c50ee84ee74d8496de8ecb550b50
Dwaipayan <biswastatay73@gmail.com>
2024-07-11 03:02:17 +0530

    updation

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,25 @@
+public class DecimalToBinary {
+
+    public static void decToBinary(int n){
+        int binaryNum[] = new int[32];
+
+        //counter
+        int i=0;
+        while (n>0){
+            binaryNum[i] = n%2;
+            n = n/2;
+            i++;
+        }
+
+        //print
+        for (int j=i-1; j>=0; j--){
+            System.out.print(binaryNum[j]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 17;
+        decToBinary(n);
+    }
+}
