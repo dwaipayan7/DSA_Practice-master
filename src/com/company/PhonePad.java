17548e6369c0dc1dc60b5c9203c6e5c3a71bb848
Dwaipayan <biswastatay73@gmail.com>
2024-07-12 02:53:50 +0530

    Google question updation

--- src/com/company/PhonePad.java
+++ src/com/company/PhonePad.java
@@ -6,6 +6,7 @@ public class PhonePad {
     public static void main(String[] args) {
 
         System.out.println(padRet("","12"));
+        System.out.println(padCount("","12"));
 
 
     }
@@ -38,5 +39,20 @@ public class PhonePad {
             list.addAll(padRet(p+ch, up.substring(1)));
         }
         return list;
+    }
+
+    static int padCount(String p, String up){
+        if (up.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0';
+
+        for (int i = (digit-1)*3; i<digit*3; i++){
+            char ch = (char) ('a' + i);
+
+            count += padCount(p+ch,up.substring(1));
+        }
+        return count;
     }
 }
