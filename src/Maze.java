4bf204e3116dc90f0006b240be257607ed1d0c16
Dwaipayan <biswastatay73@gmail.com>
2024-07-14 21:40:13 +0530

    change

--- src/Maze.java
+++ src/Maze.java
@@ -1,10 +1,21 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Maze {
     public static void main(String[] args) {
 
 //        System.out.println(count(3,3));
-        System.out.println(pathRet("",3,3));
+//        System.out.println(pathRet("",3,3));
+//        System.out.println(DiagonalpathRet("",3,3));
+
+
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true},
+        };
+
+        pathRestrictions("",board, 0, 0);
     }
 
     static int count(int r, int c){
@@ -49,4 +60,43 @@ public class Maze {
 
         return list;
     }
+
+    public static ArrayList<String> DiagonalpathRet(String p, int r, int c){
+        if (r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r>1 && c>1){
+            list.addAll(DiagonalpathRet(p+'D',r-1,c-1));
+        }
+        if (r>1){
+            list.addAll(DiagonalpathRet(p+'V',r-1,c));
+        }
+        if (c>1){
+            list.addAll(DiagonalpathRet(p+'H',r, c-1));
+        }
+        return list;
+    }
+
+
+    public static void pathRestrictions(String p, boolean[][] maze, int r, int c){
+        if (r== maze.length-1 && c== maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if (!maze[r][c]){
+            return;
+        }
+        if (r<maze.length-1){
+            pathRestrictions(p+'D',maze, r+1,c);
+        }
+        if (c<maze.length-1){
+            pathRestrictions(p+'R',maze, r,c+1);
+        }
+    }
+
+
 }
