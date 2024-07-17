d9c73d9cebc482ccc1d3c6ec98fee1f458862561
Dwaipayan <biswastatay73@gmail.com>
2024-07-17 06:34:11 +0530

    updation

--- src/com/company/NQueenPrac.java
+++ src/com/company/NQueenPrac.java
@@ -4,30 +4,57 @@ public class NQueenPrac {
 
     static int N = 4;
 
-   static boolean nQueen(int board[][], int row){
-        if (row == N){
+    static boolean nQueen(int board[][], int row) {
+        if (row == N) {
+            printBoard(board);
             return true;
         }
-        for (int col = 0; col < N-1; col++){
-            if (isSafe(board, row, col)){
+        boolean res = false; // To check if placing queen at some column leads to a solution
+        for (int col = 0; col < N; col++) {
+            if (isSafe(board, row, col)) {
                 board[row][col] = 1;
-            }
-            if (nQueen(board, row+1)){
+                res = nQueen(board, row + 1) || res;
                 board[row][col] = 0;
             }
         }
-        return false;
+        return res;
     }
 
-    public static boolean isSafe(int [][] board, int row, int col) {
+    public static boolean isSafe(int[][] board, int row, int col) {
+        int i, j;
 
-       int i,j;
+        for (i = 0; i < row; i++) {
+            if (board[i][col] == 1) {
+                return false;
+            }
+        }
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+        for (i = row, j = col; i >= 0 && j < N; i--, j++) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-       for (i=0; i<col;i++){
-           if (board[row][i] == 1){
-               return false;
-           }
-       }
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        int[][] board = new int[N][N];
+        if (!nQueen(board, 0)) {
+            System.out.println("Solution does not exist");
+        }
     }
 }
