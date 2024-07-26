afb4c853d4123d22e7153c6fc7d41729748b03d5
Dwaipayan <biswastatay73@gmail.com>
2024-07-27 02:51:20 +0530

    New update

---
+++ src/LinkedListCycleProblems.java
@@ -0,0 +1,78 @@
+public class LinkedListCycleProblems {
+
+    static Node head = null;
+
+    class Node {
+        int val;
+        Node next;
+
+        Node(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public Node insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        return node;
+    }
+
+    public boolean hasCycle(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int lengthCycle(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                int length = 0;
+                Node temp = slow;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length; // Return the length after calculating it
+            }
+        }
+        return 0; // Return 0 if there is no cycle
+    }
+
+    public static void main(String[] args) {
+        LinkedListCycleProblems list = new LinkedListCycleProblems();
+
+        // Insert nodes
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+
+        // Creating a cycle for testing
+        list.head.next.next.next.next = list.head.next; // 40 -> 30 -> 20 -> 10 -> 30 (cycle)
+
+        // Check if the list has a cycle
+        if (list.hasCycle(list.head)) {
+            System.out.println("The list has a cycle.");
+            System.out.println("The length of the cycle is: " + lengthCycle(list.head));
+        } else {
+            System.out.println("The list does not have a cycle.");
+        }
+    }
+}
