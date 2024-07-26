eb8d89d2386197c098f2a0a85f2be1e55946b04f
Dwaipayan <biswastatay73@gmail.com>
2024-07-27 02:51:20 +0530

    New update

---
+++ src/LinkedListCycleProblems.java
@@ -0,0 +1,58 @@
+public class LinkedListCycleProblems {
+
+    static Node head = null;
+
+    class Node {
+        int val;
+        Node next;
+
+        Node(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public Node insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        return node;
+    }
+
+    public boolean hasCycle(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //find the length
+    public
+
+    public static void main(String[] args) {
+        LinkedListCycleProblems list = new LinkedListCycleProblems();
+
+        // Insert nodes
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+
+        // Creating a cycle for testing
+        list.head.next.next.next.next = list.head.next; // 40 -> 30 -> 20 -> 10 -> 30 (cycle)
+
+        // Check if the list has a cycle
+        if (list.hasCycle(list.head)) {
+            System.out.println("The list has a cycle.");
+        } else {
+            System.out.println("The list does not have a cycle.");
+        }
+    }
+}
