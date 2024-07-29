8b17e9afac809610042f6ffd62875503515be9db
Dwaipayan <biswastatay73@gmail.com>
2024-07-29 15:25:32 +0530

    Changes

---
+++ src/Accenture_Coding_Test.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Accenture_Coding_Test {
+
+    public static int magicShoes(int input1, int[] input2){
+        int count = 0;
+
+        for (int i:input2){
+
+            if (i%3==0){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int input1 = 4;
+        int[] input2 = {12, 21, 3, 4};
+
+        System.out.println(Accenture_Coding_Test.magicShoes(input1, input2));
+
+
+    }
+}
