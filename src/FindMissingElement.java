198ceb129467f2c7a9374d3821d46c3897cbb5f3
Dwaipayan <biswastatay73@gmail.com>
2024-07-31 08:20:25 +0530

    Changes

---
+++ src/FindMissingElement.java
@@ -0,0 +1,22 @@
+public class FindMissingElement {
+    public static int missingNumber(int n, int[]arr){
+        int []hash = new int[n+1];
+        for (int i = 0; i<n-1; i++){
+            hash[arr[i]]++;
+        }
+        for (int i=1; i<=n;i++){
+            if (hash[i] == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int n=5;
+        int[] arr = {1,2,3,5};
+        System.out.println(missingNumber(n,arr));
+
+    }
+}
