61ca9e8a9aa69a1e56e8785923d3019c64a21801
Dwaipayan <biswastatay73@gmail.com>
2024-08-16 04:52:55 +0530

    Updated

---
+++ src/com/company/IntegerToBinary.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class IntegerToBinary {
+    public static void main(String[] args) {
+        int num = 17;
+        System.out.println(Integer.toBinaryString(num));
+    }
+}
