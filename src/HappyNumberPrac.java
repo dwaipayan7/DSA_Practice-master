4d2343d9894bbc8e8deaf9c3c43bd64e8473252f
Dwaipayan <biswastatay73@gmail.com>
2024-08-17 07:42:47 +0530

    Updated

--- src/HappyNumberPrac.java
+++ src/HappyNumberPrac.java
@@ -1,5 +1,39 @@
 public class HappyNumberPrac {
 
+    private int findSquare(int number){
+        int ans = 0;
+        while (number > 0){
+            int rem = number % 10;
+            ans += rem*rem;
+            number /= 10;
+        }
+        return ans;
+    }
 
+    public boolean isHappy(int n){
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        }while (slow != fast);
+        if (slow == 1){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        HappyNumberPrac happyNumber = new HappyNumberPrac();
+
+        int num = 19; // You can change this to any number you want to test
+
+        if (happyNumber.isHappy(num)) {
+            System.out.println(num + " is a happy number!");
+        } else {
+            System.out.println(num + " is not a happy number.");
+        }
+    }
 
 }
