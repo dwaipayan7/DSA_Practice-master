9cf505251416d8f9022d820877a2f80934de8060
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-19 06:25:26 +0530

    Updated

--- src/practice/PreviousSmaller.java
+++ src/practice/PreviousSmaller.java
@@ -19,11 +19,29 @@ public class PreviousSmaller {
         }
     }
 
+    static void nextSmaller(int[]a){
+        Stack<Integer>stack = new Stack<>();
+        for (int i = a.length-1; i>=0; i--){
+            if (!stack.isEmpty() && stack.peek() >= a[i]){
+                stack.pop();
+            }
+            if (stack.isEmpty()){
+                System.out.print(-1+" ");
+            }else{
+                System.out.print(stack.peek()+" ");
+            }
+            stack.push(a[i]);
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {10, 4, 2, 20, 40, 12, 30};
 
         System.out.println("Previous smaller elements for the array:");
         previousSmaller(arr);
+        System.out.println();
+        System.out.println("Next smaller elements for the array:");
+        nextSmaller(arr);
     }
 
 }
