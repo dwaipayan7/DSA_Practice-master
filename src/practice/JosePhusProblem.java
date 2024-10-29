ace790a404935360abf3e0c5d76952f3304ee233
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-30 05:12:52 +0530

    updated

--- src/practice/JosePhusProblem.java
+++ src/practice/JosePhusProblem.java
@@ -9,12 +9,24 @@ public class JosePhusProblem {
        return count(m-1,n) + count(m,n-1);
     }
 
-    public static void main(String[] args) {
-        int m = 3;
-        int n = 3;
-
-        System.out.println(count(m,n));
+    static int jos(int n, int k){
+        if (n==1){
+            return 0;
+        }
+        return (jos(n-1,k)+k)%n;
     }
+
+    public static void main(String[] args) {
+//        int m = 3;
+//        int n = 3;
+//
+//        System.out.println(count(m,n));
+
+        int n = 5;
+        int k = 3;
+        System.out.println(jos(n,k));
+    }
+
 
 
 }
