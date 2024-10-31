9a9543a2198d08598c6ba67d50bb40b2d563f0bd
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-01 02:50:23 +0530

    updated

--- src/practice/RecursionStrings.java
+++ src/practice/RecursionStrings.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.Scanner;
+
 public class RecursionStrings {
 
     //Palindrome
@@ -13,11 +15,29 @@ public class RecursionStrings {
         return isPalindrome(s,l+1,r-1);
     }
 
+   static void powerSet(String s, int i, String curr){
+        if (i == s.length()){
+            System.out.print(curr+" ");
+            return;
+        }
+        powerSet(s,i+1,curr+s.charAt(i));
+        powerSet(s,i+1,curr);
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String s = "mom";
         int l = 0;
         int r = s.length()-1;
-        System.out.print(isPalindrome(s,l,r));
+        System.out.print(isPalindrome(s,l,r)+"\n");
+//        System.out.println("Take input");
+//        int a = sc.nextInt();
+//        System.out.println(a);
+
+        String a = "abc";
+        powerSet(a,0,"");
+
+
     }
 
 }
