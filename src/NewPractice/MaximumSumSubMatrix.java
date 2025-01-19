18e9fc225770fd1ccbdb3508bcfb1b5ca13ebc90
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-01-19 16:59:42 +0530

    updated

---
+++ src/NewPractice/MaximumSumSubMatrix.java
@@ -0,0 +1,54 @@
+package NewPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MaximumSumSubMatrix {
+
+    static ArrayList<Integer> maxMeeting(int N, int[]S, int[] F){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[][]a = new int[N][3];
+
+        for (int i = 0; i<N; i++){
+            a[i][0] = i+1;
+            a[i][1] = S[i];
+            a[i][2] = F[i];
+        }
+
+        Arrays.sort(a, Comparator.comparingInt(o-> o[2]));
+
+        int r = a[0][2];
+
+        ans.add(a[0][0]);
+
+        for (int i = 1; i<a.length; i++){
+            if (a[i][1] > r){
+                ans.add(a[i][0]);
+                r = a[i][2];
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        // Number of meetings
+        int N = 6;
+
+        // Start times of meetings
+        int[] S = {1, 3, 0, 5, 8, 5};
+
+        // Finish times of meetings
+        int[] F = {2, 4, 6, 7, 9, 9};
+
+        // Call the maxMeeting method
+        ArrayList<Integer> result = maxMeeting(N, S, F);
+
+        // Print the result
+        System.out.println("Selected meetings: " + result);
+    }
+
+
+}
