c5df406a799f68922c277b1b5ca72e730ab00dcf
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-02-10 13:41:58 +0530

    updated

--- src/enumDemo/Main.java
+++ src/enumDemo/Main.java
@@ -2,6 +2,21 @@ package enumDemo;
 
 public class Main {
 
+    public enum Months {
+        JANUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER;
+    }
+
     public static void main(String[] args) {
 //        System.out.println("SUNDAY");
 //        System.out.println("MONDAY");
@@ -29,9 +44,50 @@ public class Main {
 //            System.out.println(i);
 //        }
 
-        Day monday = Day.MONDAY;
-//        monday.display();
-        System.out.println(monday.getLower());
+//        Day monday = Day.MONDAY;
+////        monday.display();
+//        System.out.println(monday.getLower());
+
+        Day day = Day.MONDAY;
+
+//        switch (day){
+//            case MONDAY:{
+//                System.out.println("Today is Monday");
+            //break;
+//            }
+//            case TUESDAY:{
+//                System.out.println("Today is Tuesday");
+            //break;
+//            }
+//            default:{
+//                System.out.println("Wednesday!!");
+//            }
+
+//            case MONDAY -> {
+//                System.out.println("Monday");
+//                //No Break Needed
+//            }
+//
+//            case TUESDAY -> {
+//                System.out.println("T");
+//            }
+
+            String res = switch (day){
+                case SUNDAY -> null;
+                case MONDAY -> "M";
+                case TUESDAY -> "T";
+                case WEDNESDAY -> null;
+                case THURSDAY -> null;
+                case FRIDAY -> null;
+                case SATURDAY -> null;
+                default -> "Weekend";
+            };
+
+        System.out.println(res);
+
+        System.out.println(Months.DECEMBER);
+
+
 
     }
 
