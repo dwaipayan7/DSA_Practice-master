70bb114dcbf453b360f4be75ed5c154fe42a26d8
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-02-27 14:56:31 +0530

    updated

---
+++ src/PracticeNew/HeapSort.java
@@ -0,0 +1,59 @@
+package PracticeNew;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    private static void heapify(int[]arr, int n, int i){
+        int largest = i;
+        int l = 2*i+1;
+        int r = 2*i+2;
+
+        if (l < n && arr[l] > arr[largest]){
+            largest = l;
+        }
+
+        if (r < n && arr[r] > arr[largest]){
+            largest = r;
+        }
+
+        if (largest != i){
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
+    }
+
+    private static void buildHeap(int[]arr, int n){
+
+        for (int i = n/2-1; i>=0; i--){
+            heapify(arr, n, i);
+        }
+
+    }
+
+    private static void heapSort(int[] arr, int n) {
+        buildHeap(arr, n);
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        heapSort(arr, arr.length);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
