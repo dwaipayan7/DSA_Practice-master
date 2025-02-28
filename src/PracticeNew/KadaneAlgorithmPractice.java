017c06a705a0b6c19537cc9e1c25a4afc619b9b9
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-02-28 14:38:16 +0530

    updated

---
+++ src/PracticeNew/KadaneAlgorithmPractice.java
@@ -0,0 +1,33 @@
+package PracticeNew;
+
+public class KadaneAlgorithmPractice {
+
+
+    public static int maxSumSubArray(int[]arr){
+
+        int maxSum = 0;
+        int currSum = 0;
+
+        for (int i = 1; i < arr.length; i++){
+            currSum = currSum+arr[i];
+
+            if (currSum > maxSum){
+                maxSum = currSum;
+            }
+            if (currSum < 0){
+                currSum = 0;
+            }
+
+        }
+        return maxSum;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = maxSumSubArray(arr);
+        System.out.println("Maximum sum of subarray: " + maxSum);
+    }
+
+
+}
