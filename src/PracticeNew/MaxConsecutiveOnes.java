42d84dca1ddc8f46d3315064210abc991bbce350
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-03-14 15:02:20 +0530

    updated

--- src/PracticeNew/MaxConsecutiveOnes.java
+++ src/PracticeNew/MaxConsecutiveOnes.java
@@ -22,9 +22,35 @@ public class MaxConsecutiveOnes {
         return ans;
 
     }
+
+
+    static void printMax(int[]arr, int k){
+
+        int j,max;
+        int n = arr.length;
+
+        for (int i = 0; i<n-k; i++){
+            max = arr[i];
+            for (j = 1; j<k; j++){
+                if (arr[i+j] > max){
+                    max = arr[i+j];
+                }
+            }
+            System.out.print(max+" ");
+        }
+
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {1, 1, 0, 0, 1, 1, 1};
         int k = 2;
         System.out.println(longestOnes(nums, k));
+
+        int[] arr = {1, 3, 2, 4, 5};
+        int windowSize = 3;
+        System.out.print("Sliding window maxima: ");
+        printMax(arr, windowSize);
+
     }
 }
