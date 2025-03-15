f064132b422f51aa2811530208e84483f150fff2
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-03-16 00:58:02 +0530

    updated

--- src/PracticeNew/ArrayInput.java
+++ src/PracticeNew/ArrayInput.java
@@ -1,12 +1,14 @@
 package PracticeNew;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class ArrayInput {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements: ");
         int n = sc.nextInt();
 
         int[]arr = new int[n];
--- src/PracticeNew/MinimumCardPickUp.java
+++ src/PracticeNew/MinimumCardPickUp.java
@@ -1,11 +1,29 @@
-//package PracticeNew;
-//
-//public class MinimumCardPickUp {
-//
-//    public static int minimumCardPickup(int[]cards){
-//
-//
-//
-//    }
-//
-//}
+package PracticeNew;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinimumCardPickUp {
+
+    public static int minimumCardPickup(int[]cards){
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 0; i<cards.length; i++){
+            if (map.containsKey(cards[i])){
+                res = Math.min(res, i-map.get(cards[i]) + 1);
+            }
+            map.put(cards[i], i);
+        }
+
+
+
+        return res == Integer.MAX_VALUE ? -1 : res;
+
+    }
+    public static void main(String[] args) {
+        int[] cards = {3, 4, 2, 3, 4, 7};
+        System.out.println(minimumCardPickup(cards));
+    }
+}
