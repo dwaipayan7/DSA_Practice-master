d993ae8a1e340ba18a87acce0f4704894a01340f
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-03-17 02:29:25 +0530

    updated

---
+++ src/PracticeNew/RainwaterTrappingPractice.java
@@ -0,0 +1,37 @@
+package PracticeNew;
+
+public class RainwaterTrappingPractice {
+
+
+    static int rainWater(int[]arr){
+
+        int n = arr.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        int ans = 0;
+
+        left[0] = arr[0];
+        for (int i = 1; i<n; i++){
+            left[i] = Math.max(left[i-1], arr[i]);
+        }
+
+        right[n-1] = arr[n-1];
+        for (int i = n-2; i>=0; i--){
+            right[i] = Math.max(right[i+1], arr[i]);
+        }
+
+        for (int i = 0; i<n; i++){
+            ans += Math.min(left[i], right[i]) - arr[i];
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println("Trapped Rainwater: " + rainWater(arr));
+    }
+
+}
---
+++ src/WrapperClassDemo/Test.java
@@ -0,0 +1,20 @@
+package WrapperClassDemo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 1;
+//        Integer b = 1;
+        Integer b = Integer.valueOf(1);
+         boolean hasGraphicCard = true;
+         Boolean isAdult = true;
+
+         Float f = 1.2f;;
+         Double d = 1.3;
+         Character c = 'c';
+         Byte bb = 1;
+         Short s = 2;
+         Long l = 123l;
+    }
+
+}
