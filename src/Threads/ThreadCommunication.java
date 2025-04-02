3c9590bea59ae7f6f1d0c07ea20335c0cb2d415d
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-04-02 13:09:07 +0530

    updated

--- src/Threads/ThreadCommunication.java
+++ src/Threads/ThreadCommunication.java
@@ -1,36 +1,85 @@
 package Threads;
 
-class SharedResource{
-
+class SharedResource {
     private int data;
-    private boolean hasData;
+    private boolean hasData = false;
 
-    public void produce(int value){
+    public synchronized void produce(int value) {
+        while (hasData) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
 
+        data = value;
+        hasData = true;
+        System.out.println("Produced: " + data);
+        notifyAll();  // Notify all waiting threads
     }
 
-    public void consume(){
+    public synchronized int consume() {
+        while (!hasData) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
 
+        hasData = false;
+        System.out.println("Consumed: " + data);
+        notifyAll();  // Notify all waiting threads
+        return data;
     }
-
 }
 
-class Producer implements Runnable{
-
+class Consumer implements Runnable {
     private SharedResource resource;
 
-    public Producer(SharedResource resource){
+    public Consumer(SharedResource resource) {
         this.resource = resource;
     }
 
     @Override
     public void run() {
+        for (int i = 0; i < 10; i++) {
+            resource.consume();
+        }
+    }
+}
 
+class Producer implements Runnable {
+    private SharedResource resource;
+
+    public Producer(SharedResource resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            resource.produce(i);
+        }
     }
 }
 
 public class ThreadCommunication {
+    public static void main(String[] args) {
+        SharedResource resource = new SharedResource();
 
+        Thread producerThread = new Thread(new Producer(resource));
+        Thread consumerThread = new Thread(new Consumer(resource));
 
+        producerThread.start();
+        consumerThread.start();
 
+        try {
+            producerThread.join();
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
