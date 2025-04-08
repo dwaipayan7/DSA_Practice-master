20ee042999d6bc5fefc72a79320cfd2702dd7327
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-04-08 10:06:49 +0530

    updated

---
+++ src/PracticeNew/ConnectNRopesPractice.java
@@ -0,0 +1,32 @@
+package PracticeNew;
+
+import java.util.PriorityQueue;
+
+public class ConnectNRopesPractice {
+
+    static int minCost(int[]arr){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i< arr.length; i++){
+            pq.add(arr[i]);
+        }
+
+        int ans = 0;
+
+        while (pq.size() > 1){
+            int first = pq.poll();
+            int second = pq.poll();
+            int sum = first+second;
+            ans += sum;
+            pq.add(sum);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] ropes = {4, 3, 2, 6};
+        int result = minCost(ropes);
+        System.out.println("Minimum cost to connect ropes: " + result);
+    }
+
+
+}
