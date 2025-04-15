0df200ec758771ad678bd23133ddb44af688595a
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-04-15 11:35:39 +0530

    updated

---
+++ src/arraylist/ArrayListMain.java
@@ -0,0 +1,43 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMain {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+//        System.out.println(arrayList.get(2));
+//        System.out.println(arrayList.size());
+//        for (int i = 0; i< arrayList.size(); i++){
+//            System.out.println(arrayList.get(i));
+//        }
+//
+//        for (int i : arrayList){
+//            System.out.println(i);
+//        }
+//
+//        System.out.println(arrayList.contains(5));
+//        System.out.println(arrayList.contains(1));
+
+
+        arrayList.remove(2);
+        System.out.println(arrayList.size());
+
+        arrayList.add(1, 50);
+
+        arrayList.set(2, 50);
+
+        for (int x : arrayList){
+            System.out.print(x+" ");
+        }
+
+    }
+
+}
