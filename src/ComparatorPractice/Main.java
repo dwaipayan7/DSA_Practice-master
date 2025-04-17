b2db439dd866e4538f2125316760f8de619e0425
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-04-17 12:58:39 +0530

    updated

---
+++ src/ComparatorPractice/Main.java
@@ -0,0 +1,51 @@
+package ComparatorPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class StringLengthComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+}
+
+class MyComparator implements Comparator<Integer>{
+
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+//        return o2 - o1;
+        return o1 - o2;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(3);
+
+//        list.sort(new MyComparator());
+        list.sort((a, b) -> b - a);
+        System.out.println(list);
+
+
+        List<String> words = Arrays.asList("banana", "apple", "date");
+//        words.sort(new StringLengthComparator());
+//        System.out.println(words);
+
+//        words.sort((a, b) -> a.length() - b.length());
+        words.sort((a, b) -> b.length() - a.length());
+        System.out.println(words);
+
+
+    }
+
+}
