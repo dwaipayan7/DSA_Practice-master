c59ff5e9eb84b5ff4f7bd8fefd839eb94d7e07e7
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-04-22 09:32:14 +0530

    updated

--- src/streams/LamdaExpression.java
+++ src/streams/LamdaExpression.java
@@ -1,5 +1,8 @@
 package streams;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class LamdaExpression {
 
     public static void main(String[] args) {
@@ -17,6 +20,23 @@ public class LamdaExpression {
 
         int res = sumOperation.operate(1, 2);
         System.out.println(res);
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        System.out.println(isEven.test(4));
+
+        Predicate<String> isWordStartingWithA = x -> x.toLowerCase().startsWith("a");
+        Predicate<String> isWordEndingWithT = x -> x.toLowerCase().endsWith("t");
+        Predicate<String> and = isWordStartingWithA.and(isWordEndingWithT);
+
+        System.out.println(and.test("ant"));
+
+        System.out.println(isWordStartingWithA.test("afdssa"));
+
+        //Function
+        Function<Integer, Integer> doubleIt = x -> 2 * x;
+        Function<Integer, Integer> tripleIt = x -> 3 * x;
+        Integer apply = doubleIt.andThen(tripleIt).apply(100);
+        System.out.println(apply);
 
 
     }
