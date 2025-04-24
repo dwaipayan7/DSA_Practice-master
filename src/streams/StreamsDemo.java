8e689bdea1027f01da71d2faf5d70ffb249fa11c
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-04-24 12:40:37 +0530

    updated

--- src/streams/StreamsDemo.java
+++ src/streams/StreamsDemo.java
@@ -2,6 +2,8 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
 
@@ -11,8 +13,35 @@ public class StreamsDemo {
 
         int count = 0;
 
+        for (int i : numbers){
+            if (i % 2 == 0){
+                count ++;
+            }
+        }
+
+        System.out.println(count);
         //Streams
+
+        //Source , intermediate operations  & terminal operation
         System.out.println(numbers.stream().filter(x -> x % 2 == 0).count());
+
+        //Creating Streams
+
+        //1. From collections
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        Stream<Integer> stream = list.stream();
+
+        //2. From Arrays
+        String[]array = {"a", "b", "c"};
+        //3. using Stream.of()
+        Stream<String> stream1 = Stream.of("a", "b");
+        //4. Infinite Stream
+        Stream.generate(() -> 1).limit(100);
+//        List<Integer> collect = Stream.iterate(1, x -> x + 1).limit(100).collect(Collectors.toList());
+//        System.out.println(collect);
+
+
+
 
     }
 
