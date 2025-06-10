43a377aee3bc93829c2b9489fc3cc8c0120d0cfc
Your Name <biswastatay73@gmail.com>
2025-06-10 14:02:04 +0530

    updated

---
+++ src/PracticeNew1/KadaneAlgorithmPractice.java
@@ -0,0 +1,36 @@
+package PracticeNew1;
+
+public class KadaneAlgorithmPractice {
+
+    static int maxSumSubArray(int[] arr){
+
+        int maxSum = 0;
+        int currSum = 0;
+
+        for (int i = 0; i<arr.length; i++){
+
+            currSum += arr[i];
+
+            if (currSum > maxSum){
+                maxSum = currSum;
+            }
+
+            if (currSum < 0){
+                currSum = 0;
+            }
+
+        }
+
+        return maxSum;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+
+        int result = maxSumSubArray(arr);
+
+        System.out.println("Maximum Subarray Sum is: " + result);
+    }
+
+}
