d858e646c5efe03ec7d2072725f8b8fa6650ea5e
Your Name <biswastatay73@gmail.com>
2025-06-20 19:37:47 +0530

    updated

---
+++ src/PracticeNew1/SearchInfinityPractice.java
@@ -0,0 +1,54 @@
+package PracticeNew1;
+
+public class SearchInfinityPractice {
+
+    static int searchInfinity(int[]arr, int key) {
+
+        int low = 0;
+        int high = 1;
+
+        while (arr[high] < key){
+            low = high;
+            high = 2 * high;
+        }
+
+        return binarySearch(arr, key, low, high);
+    }
+
+    static int binarySearch(int[]arr, int key, int low, int high){
+
+        low = 0;
+        high = arr.length;
+
+        while (low <= high){
+            int mid = low + (high-low)/2;
+
+            if (arr[mid] == key){
+                return mid;
+            } else if (arr[mid] < key) {
+
+                low = mid+1;
+
+            }else{
+                high = mid-1;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 15, 20, 35, 50, 60, 75, 90, 100};
+        int key = 75;
+
+        int result = searchInfinity(arr, key);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+
+}
