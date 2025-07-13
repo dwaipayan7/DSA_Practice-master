2a15f099d33fa7dd38add5a013a0ca5cb9e2cd01
Your Name <biswastatay73@gmail.com>
2025-07-13 18:47:15 +0530

    updated

---
+++ src/Enum/Day.java
@@ -0,0 +1,12 @@
+package Enum;
+
+public enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
---
+++ src/Enum/Main.java
@@ -0,0 +1,17 @@
+package Enum;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Day.MONDAY);
+        System.out.println(Day.THURSDAY);
+        System.out.println(Day.MONDAY);
+
+        Day monday = Day.MONDAY;
+        System.out.println(monday.ordinal());
+        System.out.println(monday.name().toLowerCase());
+        Day enumDay = Day.valueOf("MONDAY");
+    }
+
+}
