6aec1961b3251f7efbe7e05e9bb194bd24dece5d
Dwaipayan Biswas <biswastatay73@gmail.com>
2026-07-23 23:30:27 +0530

    added

---
+++ src/Practice01/StockBuySell2.java
@@ -0,0 +1,27 @@
+package Practice01;
+
+public class StockBuySell2 {
+
+    static int maxProfit(int []arr){
+        int profit = 0;
+
+        for (int i = 1; i<arr.length; i++ ){
+            if (arr[i] > arr[i-1]){
+                profit += (arr[i] - arr[i-1] );
+            }
+        }
+
+
+        return profit;
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 2, 7, 3, 6, 1, 2, 4};
+        System.out.println(maxProfit(arr));
+
+    }
+
+
+}
